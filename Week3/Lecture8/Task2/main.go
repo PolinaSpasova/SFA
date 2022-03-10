11f43be1af55ce2372ce5b30124495dcc5deff51
Polina <polinaspasova@abv.bg>
2022-03-10 22:49:31 +0200

    L8T2

---
+++ Week3/Lecture8/Task2/main.go
@@ -0,0 +1,49 @@
+package main
+
+import("fmt")
+
+type Square struct{
+	a int
+}
+
+func NewSquare(a int) *Square{
+	return &Square{a:a}
+}
+
+func (r *Square) Area() float64 {
+	return float64(r.a)*float64(r.a)
+}
+
+type Circle struct{
+	r int
+}
+
+func NewCircle(r int) *Circle{
+	return &Circle{r:r}
+}
+
+func (r *Circle) Area() float64 {
+	return float64(r.r)*float64(r.r)*3.14
+}
+
+type Shape interface{
+	Area() float64
+}
+
+type Shapes []Shape
+
+func (s Shapes) LargestArea() float64{
+	max:=s[0].Area()
+	for i:=1;i<len(s);i++{
+		if max<s[i].Area() {
+			max=s[i].Area()
+		}
+	}
+	return max
+}
+
+func main() {
+	var try Shapes
+	try=append(try,NewCircle(3),NewSquare(6),NewSquare(7),NewCircle(12))
+	fmt.Println(try.LargestArea())
+}
