bb626baeac1473dd75b4306e5ae1eefd3a274af7
Polina <polinaspasova@abv.bg>
2022-03-15 20:08:29 +0200

    L9T1

---
+++ Week3/Lecture9/Task1/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/polinaspasova/Week3/Lecture9/Task1/carddrawTwo"
+	"github.com/polinaspasova/Week3/Lecture9/Task1/cardgameTwo"
+)
+
+func main() {
+
+	deck := cardgameTwo.Deck{}
+	fmt.Println(deck)
+	fmt.Print("\n\n")
+	//	fmt.Println(deck.Deal())
+	res, err := carddrawTwo.DrawAllCards(&deck)
+	if err != nil {
+		
+		log.Fatalf("Error %s occured ", err)
+	} else {
+		fmt.Println(res)
+	}
+}
