7010cc9c46d177207710444f19fae0141f75cb33
Polina <polinaspasova@abv.bg>
2022-05-17 10:30:35 +0300

    L29

---
+++ Week10/Lecture29/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+type Order struct {
+	Customer string
+	Amount   int
+}
+
+func GroupBy[T any, U comparable](col []T, keyFn func(T) U) map[U][]T {
+	outs:=make(map[U][]T,len(col))
+
+	for _, item := range col {
+		outs[keyFn(item)]=append(outs[keyFn(item)], item)
+	}
+
+	return outs
+}
+
+func main() {
+
+	new := []Order{
+		{Customer: "John", Amount: 1000},
+		{Customer: "Sara", Amount: 2000},
+		{Customer: "Sara", Amount: 1800},
+		{Customer: "John", Amount: 1200},
+	}
+
+	results := GroupBy(new, func(o Order) string { return o.Customer })
+
+	fmt.Println(results)
+}
